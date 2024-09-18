ed8042bfcaf74767a3235fdae8fa93b397027e1e
Shahriar Sabuz <mksssqa@gmail.com>
2024-09-18 22:50:21 +0600

    test-output-thread folder removed

--- test-output-thread/report.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-CucumberHTML.timelineItems.pushArray([{"id":"fa3ea851-39cd-4de6-9230-6f99d5ff33a1","feature":"Remove item from cart","scenario":"Verify Text “Nothing to see here” on cart screen after removing items","start":1726257752284,"group":1,"content":"","tags":"","end":1726257859707,"className":"failed"}]);
-CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[#1,main,5,main]"}]);
-});
